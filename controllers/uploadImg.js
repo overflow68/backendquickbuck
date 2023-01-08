9af3096446002e25f0f74531fe588a16f6f198dc
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2023-01-08 04:54:53 +0000

    s

--- controllers/uploadImg.js
+++ controllers/uploadImg.js
@@ -27,33 +27,9 @@ const uploadImagesOld = async(req,res,next)=>{
     }
 
     const uploadImages = async(req,res,next)=>{
-        tmp.file((err, path, fd, cleanup) => {
-            if (err) {
-              console.error(`An error occurred while creating the temporary file: ${err}`);
-            } else {
-              console.log(`Successfully created the temporary file: ${path}`);
-          
-              // Write the image data to the temporary file.
-              fs.writeFile(path, req.files.image.data, (writeErr) => {
-                if (writeErr) {
-                  console.error(`An error occurred while writing to the temporary file: ${writeErr}`);
-                } else {
-                  console.log(`Successfully added the image data to the temporary file`);
-          
-                  cloudinary.uploader.upload(path)
-                  .then(function(image) {
-                    // Set req.body.image to the secure URL of the image
-                    req.body.image = image.secure_url;
-                    res.status(200).json({pic:image.secure_url})
-                  })
-                  .catch(function(error) {
-                    console.log(error);
-                  });
-                  
-                }
-              });
-            }
-          });
+        
+                    res.status(200).json({pic:"pac"})
+                 
         }
     
 
